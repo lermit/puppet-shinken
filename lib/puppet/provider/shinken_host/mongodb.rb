54231d8d17da0acc9433b512c00e2e0ce3246292
Romain THERRAT <romain42@gmail.com>
2014-11-11 14:59:17 +0100

    Mod: better confinment for mongo providers

--- lib/puppet/provider/shinken_host/mongodb.rb
+++ lib/puppet/provider/shinken_host/mongodb.rb
@@ -1,6 +1,9 @@
 require 'puppet/provider/shinken_mongodb'
 
 Puppet::Type.type(:shinken_host).provide :mongodb, :parent => Puppet::Provider::Shinken_mongodb do
+  confine :feature => :mongo
+  confine :shinken_mongo => :true
+  defaultfor :shinken_mongo => :true
 
   def collection_name
     'hosts'
--- lib/puppet/provider/shinken_mongodb.rb
+++ lib/puppet/provider/shinken_mongodb.rb
@@ -4,7 +4,6 @@ require 'mongo' if Puppet.features.mongo?
 include Mongo
 
 class Puppet::Provider::Shinken_mongodb < Puppet::Provider::Shinken
-  confine :feature => :mongo
 
   @@collection = nil
 
