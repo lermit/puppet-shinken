37d73c929c2826803542377c478adedcf1f80a56
Romain THERRAT <romain42@gmail.com>
2014-11-12 14:25:30 +0100

    Mod: fix shinken::module::mongo tests

--- spec/classes/shinken_module_mongo_spec.rb
+++ spec/classes/shinken_module_mongo_spec.rb
@@ -9,14 +9,14 @@ describe 'shinken::module::mongodb' do
   describe 'Test standard installation' do
     it { should contain_shinken__module__config('mongodb').with_ensure('present') }
 
-    it { should contain_file('shinken-module-mongodb.cfg').with_content(/module_type mongodb/) }
-    it { should contain_file('shinken-module-mongodb.cfg').with_content(/module_name mongodb/) }
-    it { should contain_file('shinken-module-mongodb.cfg').with_content(/uri mongodb:\/\/localhost\/\?safe=false/) }
-    it { should contain_file('shinken-module-mongodb.cfg').with_content(/database shinken/) }
+    it { should contain_file('shinken-config-module-mongodb').with_content(/module_type mongodb/) }
+    it { should contain_file('shinken-config-module-mongodb').with_content(/module_name mongodb/) }
+    it { should contain_file('shinken-config-module-mongodb').with_content(/uri mongodb:\/\/localhost\/\?safe=false/) }
+    it { should contain_file('shinken-config-module-mongodb').with_content(/database shinken/) }
 
-    it { should_not contain_file('shinken-module-mongodb.cfg').with_content(/username/) }
-    it { should_not contain_file('shinken-module-mongodb.cfg').with_content(/password/) }
-    it { should_not contain_file('shinken-module-mongodb.cfg').with_content(/replica_set/) }
+    it { should_not contain_file('shinken-config-module-mongodb').with_content(/username/) }
+    it { should_not contain_file('shinken-config-module-mongodb').with_content(/password/) }
+    it { should_not contain_file('shinken-config-module-mongodb').with_content(/replica_set/) }
   end
 
   describe 'Test username/password args' do
@@ -24,14 +24,14 @@ describe 'shinken::module::mongodb' do
       :username => 'user42',
       :password => 'pass42',
     } }
-    it { should contain_file('shinken-module-mongodb.cfg').with_content(/username user42/) }
-    it { should contain_file('shinken-module-mongodb.cfg').with_content(/password pass42/) }
+    it { should contain_file('shinken-config-module-mongodb').with_content(/username user42/) }
+    it { should contain_file('shinken-config-module-mongodb').with_content(/password pass42/) }
   end
 
   describe 'Test replica_set args' do
     let(:params) { {
       :replica_set => 'repl42',
     } }
-    it { should contain_file('shinken-module-mongodb.cfg').with_content(/replica_set repl42/) }
+    it { should contain_file('shinken-config-module-mongodb').with_content(/replica_set repl42/) }
   end
 end
