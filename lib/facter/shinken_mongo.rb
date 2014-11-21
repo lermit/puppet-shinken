c19770535a8702ecb77c147f8f596c8adb77e2fb
Romain THERRAT <romain42@gmail.com>
2014-11-21 16:32:49 +0100

    Mod: Fix shinken_mongo fact

--- lib/facter/shinken_mongo.rb
+++ lib/facter/shinken_mongo.rb
@@ -1,6 +1,6 @@
 Facter.add("shinken_mongo") do
   setcode do
-    if File.file?('/etc/shinken/arbiters/arbiter-master.cfg') and File.open('/etc/shinken/arbiters/arbiter-master.cfg').read() =~ /^\s*module[^#]*Mongodb/
+    if File.file?('/etc/shinken/arbiters/arbiter-master.cfg') and File.open('/etc/shinken/arbiters/arbiter-master.cfg').read() =~ /^\s*modules[^#]*mongodb/
       true
     else
       false
